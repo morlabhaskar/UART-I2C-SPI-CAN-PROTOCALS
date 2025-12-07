ec7de5adbd74bb72726839d2675d4c1a4665db1d
morlabhaskar <morlabhaskar306@gmail.com>
2025-12-08 00:21:33 +0530

    Add files

---
+++ UART/uart_defines.h
@@ -0,0 +1,34 @@
+#ifndef __UART_DEFINES_H__ 
+#define __UART_DEFINES_H__
+
+//UART pin config defines
+#define PIN_FUNC1 0
+#define PIN_FUNC2 1
+#define PIN_FUNC3 2
+#define PIN_FUNC4 3
+
+#define TXDO_PIN_0_1 PIN_FUNC2
+#define RXDO_PIN_0_1 PIN_FUNC2
+
+//clock and Baud Rate defines
+#define FOSC 12000000
+#define CCLK (FOSC*5)
+#define PCLK (CCLK/4)
+#define BAUD 9600
+#define DIVISOR (PCLK/(16*BAUD))
+// #define 8BIT 3
+//definition or determine the word length
+/*
+00  →  5 bit data
+01  →  6 bit data
+10  →  7 bit data
+11  →  8 bit data
+*/
+//so 3(binary 11) selects 8-bit data mode
+#define WORD_LEN 3
+#define DLAB_BIT 7
+#define RDR_BIT 0
+#define THRE_BIT 5
+#define TEMT_BIT 6
+
+#endif
